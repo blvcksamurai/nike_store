21f5c76bc8e13b497459e15f3ecf2822673f105e
blvcksamurai <theblvcksamurai@gmail.com>
2024-01-31 23:22:03 +0100

    Home Screen Update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -164,6 +164,9 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(height: 20),
             BigText(text: 'Best Sellers'),
             SizedBox(height: 25),
+            SizedBox(height: 20),
+            BigText(text: 'Best Sellers'),
+            SizedBox(height: 25),
             BestSellersItem(
               image: 'assets/images/air_mid.png',
               gender: 'Women',
