981104f34e0cfbb93a7d65fd606a8d283c3bf248
blvcksamurai <theblvcksamurai@gmail.com>
2024-02-02 22:40:32 +0100

    Home Screen Update

--- lib/widgets/best_seller_item.dart
+++ lib/widgets/best_seller_item.dart
@@ -77,17 +77,12 @@ class BestSellersItem extends StatelessWidget {
                         style: TextStyle(
                           color: Color(0xFF2C2C2C),
                           fontSize: 12,
-                          
                           fontFamily: 'Product Sans',
                           fontWeight: FontWeight.w400,
                           height: 0,
                         ),
                       ),
                       SvgPicture.asset('assets/images/rating.svg')
-                                            SvgPicture.asset('assets/images/rating.svg')
-
-
-
                     ],
                   ),
                   Row(
