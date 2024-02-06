c37cfa5b1be6958d033f58f8aaccc3f943f7b190
blvcksamurai <theblvcksamurai@gmail.com>
2024-02-06 14:56:24 +0100

    Kick selection screen update

--- lib/widgets/counter.dart
+++ lib/widgets/counter.dart
@@ -1,1 +1,52 @@
+import 'package:flutter/material.dart';
 
+class CounterScreen extends StatefulWidget {
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        IconButton(
+            onPressed: _decrementCounter,
+            icon: Icon(
+              Icons.remove_circle_outline,
+              color: Colors.black,
+              size: 30,
+            )),
+        AnimatedOpacity(
+          opacity: _counter != 0 ? 1.0 : 0.0,
+          duration: Duration(milliseconds: 500),
+          child: Text(
+            '$_counter',
+            style: TextStyle(
+              color: Color(0xFF090909),
+              fontSize: 18,
+              fontFamily: 'Product Sans',
+              fontWeight: FontWeight.w400,
+              height: 0,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
