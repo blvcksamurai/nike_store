04ce3ed27787c846bba8ccc89b135d534d56ddfc
blvcksamurai <theblvcksamurai@gmail.com>
2024-02-08 22:21:20 +0100

    Kick selection screen update

--- lib/screens/shoe_screen.dart
+++ lib/screens/shoe_screen.dart
@@ -170,8 +170,6 @@ class ShowScreen extends StatelessWidget {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               KickPicker(image: 'assets/images/s1.png'),
-                              KickPicker(image: 'assets/images/s1.png'),
-                              KickPicker(image: 'assets/images/s1.png'),
                               KickPicker(image: 'assets/images/s2.png'),
                               KickPicker(image: 'assets/images/s3.png'),
                               KickPicker(image: 'assets/images/s4.png'),
